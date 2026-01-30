1e5799606816e9cd7ae7a639407b217913ed8cd9
schneiterdogs-cmyk <schneiterdogs@gmail.com>
2026-01-30 09:18:26 -1200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // --- CONFIGURAZIONE GLOBALE ---
 const CONFIG = {
     // Cambia questo URL una sola volta per aggiornare tutto il sito
-    URL_SHEETS: "https://script.google.com/macros/s/AKfycbwEKcU5wYv9lijMhW0T6LipR0knipDSo7HtNhnYpRdcC8ObEIRdcrtNI0gjh815OU0hng/exec"
+    URL_SHEETS: "https://script.google.com/macros/s/AKfycbzgvOQptOH_ot-AESJqvPqehYA_zlgrl8ujhRJG_nV6lhrcmayAcOvHCx5_RaJkSBSrJw/exec"
 };
 
 // Funzione per collegare l'URL ai form in automatico
